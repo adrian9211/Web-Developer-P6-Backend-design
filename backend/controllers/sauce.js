87f1043d3f7aa7e6fb395a1980e469e36f089952
adrian9211 <adrian9211@gmail.com>
2021-01-18 21:30:09 +0000

    Like feature updated

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -19,8 +19,8 @@ exports.createSauce = (req, res, next) => {                                     
         heat: req.body.sauce.heat,
         likes: req.body.sauce.likes,
         dislikes: req.body.sauce.dislikes,
-        userLiked: req.body.sauce.userLiked,
-        userDisliked: req.body.sauce.userDisliked
+        userLiked: req.body.sauce.userLiked,                                      // Array created to see id of user who liked product
+        userDisliked: req.body.sauce.userDisliked                                 // Array created to see id of user who disliked product
     });
 
     sauce.save().then(
@@ -75,56 +75,49 @@ exports.viewSauce = (req, res, next) => {                                      /
 };
 
 exports.updateSauce = (req, res, next) => {
-
-    let sauce = new Sauce({ _id: req.params._id });
-    if (req.file) {
+    Sauce.findOne({ _id: req.params.id })
+    .then(sauce => {
+      if (req.file) {
         const url = req.protocol + '://' + req.get('host');
-        req.body.sauce = JSON.parse(req.body.sauce);
+        req.body.sauce = JSON.parse(req.body.sauce)
         sauce = {
-            _id: req.params._id,
-            name : req.body.sauce.name,
-            manufacturer : req.body.sauce.manufacturer,
-            description : req.body.sauce.description,
-            imageUrl : url + '/images/' + req.file.filename,
-            heat : req.body.sauce.heat,
-            userId : req.body.sauce.userId,
-            //likes : req.body.sauce.likes,
-           // dislikes : req.body.sauce.dislikes,
-            //usersDisliked : req.body.sauce.usersDisliked,
-            //usersLiked : req.body.sauce.usersLiked
-        };
-    } else{
+          _id: req.params.id,
+          userId: req.body.sauce.userId,
+          name: req.body.sauce.name,
+          description: req.body.sauce.description,
+          manufacturer: req.body.sauce.manufacturer,
+          imageUrl: url + req.file.key,                         // Didn't attached into else section because do not require update image
+          heat: req.body.sauce.heat
+        }
+      } else {
         sauce = {
-            _id: req.params._id,
-            name : req.body.name,
-            manufacturer : req.body.manufacturer,
-            description : req.body.description,
-            imageUrl : req.body.imageUrl,
-            heat : req.body.heat,
-            userId : req.body.userId,
-        };
-    }
-    Sauce.updateOne({_id: req.params.id}, sauce).then(
-        () => {
-            res.status(201).json({
-                message: 'Individual Sauce - Product Updated!'
-            });
+          _id: req.params.id,
+          userId: req.body.userId,
+          name: req.body.name,
+          description: req.body.description,
+          manufacturer: req.body.manufacturer,
+          heat: req.body.heat
         }
-    ).catch(
-        (error) => {
-            res.status(400).json({
-                error: error
-            });
-        }
-    );
-
-    console.log("Update Individual Sauce - Product");
-};
-
+      }
+  
+      Sauce.updateOne({ _id: req.params.id }, sauce)
+        .then(() => {
+          res.status(201).json({
+            message: 'Product updated!'
+          })
+        })
+        .catch(error => {
+          res.status(400).json({
+            error: error
+          })
+        })
+    })
+  }
 exports.deleteSauce = (req, res, next) => {
 
-    Sauce.findOne({_id: req.params.id}).then(
-        (sauce)=>{
+    Sauce.findOne({
+        _id: req.params.id
+            }).then((sauce)=>{
             const filename = sauce.imageUrl.split('/images/')[1];
             fs.unlink('images/' + filename, () => {                             // fs - access to filesystem - required to delete img from local folder
                     Sauce.deleteOne({_id: req.params.id}).then(
@@ -148,42 +141,46 @@ exports.deleteSauce = (req, res, next) => {
     console.log("Delete Individual Sauce - Product");
 };
 
-exports.likeSauce = async(req, res, next) => {
-
-    const like = await(req.body.like);
-    //number sent from front end (1 or 0 or -1)
-      console.log("like is "+like);
-        switch(like) {
-          /*******like******/
-          case '1':
-          // console.log("case 1");
-            Sauce.findOneAndUpdate({_id: req.params.id}, 
-              function(err, result) {
-              
-              if (err) {
-                res.send(err);
-              } else {
-                res.send(result);
-              }
-              });
-              break;
-            /**********dislike**********/
-           case '-1':
-             //console.log("case -1");
-              Sauce.findOneAndUpdate({_id: req.params.id},
-                {$addToSet:{usersDisliked: currentUser}, $inc:{ dislikes: 1 }}, {new: true}, function(err, result) {
-                  
-                if (err) {
-                  res.send(err);
-                } else {
-                  res.send(result);
-                }
-                });
-               break;
-          default:
-          console.log("default");
-        }
+exports.likeSauce = (req, res, next) => {
+    req.body = req.body
+    Sauce.findOne({ 
+        _id: req.params.id 
+    }).then(sauce => {
+        // User liked and description
+      if (req.body.like == 1) {
+        sauce.usersLiked.push(req.body.userId)
+        sauce.likes += req.body.like
+      } 
+        else if (
+        req.body.like == 0 &&
+        sauce.usersLiked.includes(req.body.userId)
+      ) {
+        sauce.usersLiked.remove(req.body.userId)
+        sauce.likes -= 1
+      } 
+    //   User disliked and description
+      else if (req.body.like == -1) {
+        sauce.usersDisliked.push(req.body.userId)
+        sauce.dislikes += 1
+      } 
+      else if (
+        req.body.like == 0 &&
+        sauce.usersDisliked.includes(req.body.userId)
+      ) {
+        sauce.usersDisliked.remove(req.body.userId)
+        sauce.dislikes -= 1
       }
+      sauce.save().then(() => {
+          res.status(201).json({
+            message: 'Preference sended to the system.'
+          })
+        }).catch(error => {
+            res.status(400).json({
+            error: error
+          })
+        })
+    })
+  }
 
     
       
